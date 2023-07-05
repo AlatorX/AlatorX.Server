d6400216d7c48bb8d7580f67168f49d4db3a07f6
mtukhtaboev <75162831+MrTukhtaboev@users.noreply.github.com>
2023-07-05 11:16:21 +0500

    Update Website

--- AlatorX.Server.Management/Service/Services/WebsiteService.cs
+++ AlatorX.Server.Management/Service/Services/WebsiteService.cs
@@ -1,7 +1,10 @@
 using AlatorX.Server.Management.Data.IRepositories;
 using AlatorX.Server.Management.Domain.Entities;
 using AlatorX.Server.Management.Service.DTOs.Websites;
+using AlatorX.Server.Management.Service.Exceptions;
+using AlatorX.Server.Management.Service.Helpers;
 using AlatorX.Server.Management.Service.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace AlatorX.Server.Management.Service.Services
 {
@@ -18,10 +21,33 @@ namespace AlatorX.Server.Management.Service.Services
         {
             var website = await _websiteRepository.InsertAsync(new Website
             {
-                Name = dto.Name
+                Name = dto.Name,
+                UserId = HttpContextHelper.UserId ?? throw new UnauthorizedAccessException(),
+                ConfigString = dto.ConfigString
             });
 
             return website;
+        }
+
+        public async ValueTask<Website> ModifyAsync(long id, WebsiteForCreationDto dto)
+        {
+            var website = await _websiteRepository.SelectByIdAsync(id);
+            if(website == null)
+                throw new AlatorException(404, "Website not found");
+
+            website.ConfigString = dto.ConfigString;
+            website.Name = dto.Name;
+            website.UpdatedAt = DateTime.UtcNow;
+
+            await _websiteRepository.SaveChangesAsync();
+
+            return website;
+        }
+
+        public async ValueTask<IEnumerable<Website>> RetrieveAllAsync()
+        {
+            return await _websiteRepository.SelectAll()
+                .ToListAsync();
         }
     }
 }
